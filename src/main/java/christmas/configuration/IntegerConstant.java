bde4d6b5047ee8fa606ae7bc78c7a8e2f338dd45
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 05:42:16 +0900

    feat:IntegerConstant 추가

---
+++ src/main/java/christmas/configuration/IntegerConstant.java
@@ -0,0 +1,16 @@
+package christmas.configuration;
+
+public enum IntegerConstant {
+    MAX_QUANTITY(20),
+    MIN_DAY(1),
+    MAX_DAY(31);
+
+    private final int value;
+    IntegerConstant(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
