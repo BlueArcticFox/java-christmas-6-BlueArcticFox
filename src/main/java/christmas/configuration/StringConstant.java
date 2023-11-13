b5940da75bae937153ee86ea700747be0745d1f7
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 16:46:34 +0900

    feat:Food Section에 대한 상수 추가

--- src/main/java/christmas/configuration/StringConstant.java
+++ src/main/java/christmas/configuration/StringConstant.java
@@ -2,7 +2,12 @@ package christmas.configuration;
 
 public enum StringConstant {
     DELIMITER_COMMA(","),
-    DELIMITER_DASH("-");
+    DELIMITER_DASH("-"),
+    APPETIZER("애피타이저"),
+    MAIN_DISH("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+
 
     private final String value;
     StringConstant(String value) {
