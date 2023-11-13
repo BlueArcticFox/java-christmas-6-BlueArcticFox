dfe8ecd75a11d13214f787d1a8897d8031a6c792
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 17:29:10 +0900

    feat:총 가격 계산 및 반환 기능 및 음료만 주문시 예외 발생 기능 추가

--- src/main/java/christmas/service/OrdersService.java
+++ src/main/java/christmas/service/OrdersService.java
@@ -1,10 +1,12 @@
 package christmas.service;
 
 import static christmas.configuration.StringConstant.DELIMITER_DASH;
+import static christmas.configuration.StringConstant.DRINK;
 import static christmas.message.ErrorMessage.INVALID_MENU_MESSAGE;
 
 import christmas.domain.entity.Menu;
 import christmas.domain.entity.Orders;
+import christmas.util.ExceptionUtil;
 import christmas.util.IntegerUtil;
 import christmas.util.MenuUtil;
 import christmas.util.StringUtil;
@@ -22,7 +24,23 @@ public class OrdersService {
 
             orders.addOrder(menu, quantity);
         }
+        validateAllDrinks(orders);
         return orders;
+    }
+
+    private void validateAllDrinks(Orders orders) {
+        if(getNumberOfOrders(orders) == getNumberOfSection(orders, DRINK.getValue())) {
+            ExceptionUtil.throwIllegalArgumentException(INVALID_MENU_MESSAGE.getMessage());
+        }
+    }
+
+    public int getTotalPrice(Orders orders) {
+        int prize = 0;
+        Map<Menu, Integer> orderGroup = orders.getOrders();
+        for(Menu menu: orderGroup.keySet()) {
+            prize += menu.getTotalPrice(orderGroup.get(menu));
+        }
+        return prize;
     }
 
     public int getNumberOfOrders(Orders orders) {
