5949bb1f2840284f7cd5b9d788526f67f5f56f6f
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 16:28:02 +0900

    test:요일관련 Test 추가

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -1,7 +1,9 @@
 package christmas.domain;
 
+import static christmas.configuration.DateConstant.TUESDAY;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.domain.entity.VisitDate;
 import org.junit.jupiter.api.DisplayName;
@@ -20,5 +22,12 @@ public class DateTest {
     void badAddOrderTest_1() {
         assertThatThrownBy(() -> new VisitDate(50))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("요일 테스트")
+    @Test
+    void weekTest() {
+        VisitDate visitDate = new VisitDate(12);
+        assertEquals(visitDate.getWeek(), TUESDAY.getValue());
     }
 }
