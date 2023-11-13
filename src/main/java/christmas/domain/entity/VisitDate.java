fe58d25f5b8fe19509924766cec1f0dfc7608f14
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 16:27:54 +0900

    feat:요일관련 기능 추가

--- src/main/java/christmas/domain/entity/VisitDate.java
+++ src/main/java/christmas/domain/entity/VisitDate.java
@@ -1,16 +1,29 @@
 package christmas.domain.entity;
 
+import static christmas.configuration.DateConstant.DECEMBER_FIRST_WEEK_WEIGHT;
+import static christmas.configuration.DateConstant.WEEKS_NUMBER;
+
 import christmas.validation.VisitDateValidator;
 
 public class VisitDate extends VisitDateValidator {
     private final int date;
+    private final int week;
 
     public VisitDate(int date) {
         validateDate(date);
         this.date = date;
+        week = decideWeek();
+    }
+
+    public int decideWeek() {
+        int weight = date + DECEMBER_FIRST_WEEK_WEIGHT.getValue();
+        return weight % WEEKS_NUMBER.getValue();
     }
 
     public int getDate() {
         return date;
+    }
+    public int getWeek() {
+        return week;
     }
 }
