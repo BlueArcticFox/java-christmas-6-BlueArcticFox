2117e59c4f2b042fe05494c72280b04827554726
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 19:21:33 +0900

    chore:클래스 이름 변경

--- src/main/java/christmas/domain/entity/Giveaway.java
+++ src/main/java/christmas/domain/entity/Giveaway.java
@@ -2,13 +2,12 @@ package christmas.domain.entity;
 
 import static christmas.domain.entity.Menu.CHAMPAGNE;
 
-public enum GiveawayCondition {
+public enum Giveaway {
     GIVEAWAY_0(CHAMPAGNE, 120000);
 
     private final Menu menu;
     private final int condition;
-    private
-    GiveawayCondition(Menu menu, int condition) {
+    private Giveaway(Menu menu, int condition) {
         this.menu = menu;
         this.condition = condition;
     }
