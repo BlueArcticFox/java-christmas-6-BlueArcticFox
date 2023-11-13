c3b732a56c7d357baca7fc9e00e2e3ae42e98ab2
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-13 19:23:25 +0900

    test:Promotion Test 추가

---
+++ src/test/java/christmas/domain/PromotionTest.java
@@ -0,0 +1,37 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import christmas.domain.entity.Orders;
+import christmas.domain.entity.Promotion;
+import christmas.domain.entity.VisitDate;
+import christmas.domain.entity.discount.MyDiscount;
+import christmas.service.OrdersService;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PromotionTest {
+    private OrdersService ordersService;
+    private Orders orders;
+    private VisitDate visitDate;
+    private Promotion promotion;
+    @BeforeEach
+    void setup() {
+        ordersService = new OrdersService();
+        visitDate = new VisitDate(24);
+        orders = ordersService.generateOrders(List.of(
+                "타파스-3",
+                "티본스테이크-2",
+                "아이스크림-4"
+        ));
+        promotion = new Promotion(visitDate, orders);
+    }
+
+    @DisplayName("전체 혜택 금액 테스트")
+    @Test
+    void promotionTest_1() {
+       assertEquals(promotion.getTotalBenefit(), 12392+25000);
+    }
+}
