43ab0538419762662628fdec7c45fc7fb6b72886
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-14 04:18:17 +0900

    feat:OutputView 기능 모두 구현

--- src/main/java/christmas/controller/EventPlanerController.java
+++ src/main/java/christmas/controller/EventPlanerController.java
@@ -1,8 +1,15 @@
 package christmas.controller;
 
+import christmas.domain.dto.OrderPriceDto;
+import christmas.domain.dto.OrdersDto;
+import christmas.domain.dto.PromotionDto;
 import christmas.domain.entity.Orders;
+import christmas.domain.entity.Promotion;
 import christmas.domain.entity.VisitDate;
 import christmas.service.OrdersService;
+import christmas.service.OrdersServiceImpl;
+import christmas.service.PromotionService;
+import christmas.service.PromotionServiceImpl;
 import christmas.view.ConsoleInputView;
 import christmas.view.ConsoleOutputView;
 import christmas.view.InputView;
@@ -10,22 +17,30 @@ import christmas.view.OutputView;
 import java.util.List;
 
 public class EventPlanerController {
-    InputView inputView = new ConsoleInputView();
-    OutputView outputView = new ConsoleOutputView();
-    OrdersService ordersService = new OrdersService();
+    private final InputView inputView = new ConsoleInputView();
+    private final OutputView outputView = new ConsoleOutputView();
+    private final OrdersService ordersService = new OrdersServiceImpl();
+    private final PromotionService promotionService = new PromotionServiceImpl();
 
     public void runEventPlaner() {
         outputView.printWelcome();
-        VisitDate date = decideDate();
+        VisitDate visitDate = decideVisitDate();
         Orders orders = decideOrders();
+        Promotion promotion = promotionService.generatePromotion(visitDate, orders);
+
+        OrdersDto ordersDto = ordersService.generateOrdersDto(visitDate, orders.getOrders());
+        OrderPriceDto orderPriceDto = promotionService.generateOrderPriceDto(orders, promotion);
+        PromotionDto promotionDto = promotionService.generatePromotionDto(promotion);
+
+        printResult(ordersDto, orderPriceDto, promotionDto);
     }
 
-    private VisitDate decideDate() {
+    private VisitDate decideVisitDate() {
         try {
             int input = inputView.inputDate();
             return new VisitDate(input);
         } catch (IllegalArgumentException e) {
-            return decideDate();
+            return decideVisitDate();
         }
     }
 
@@ -36,5 +51,16 @@ public class EventPlanerController {
         } catch (IllegalArgumentException e) {
             return decideOrders();
         }
+    }
+
+    private void printResult(OrdersDto ordersDto, OrderPriceDto orderPriceDto, PromotionDto promotionDto) {
+        outputView.printPromotionPreview(ordersDto);
+        outputView.printOrders(ordersDto);
+        outputView.printTotalPriceBeforeDiscount(orderPriceDto);
+        outputView.printGiveaway(promotionDto);
+        outputView.printBenefit(promotionDto);
+        outputView.printTotalBenefit(orderPriceDto);
+        outputView.printExpectedPriceAfterDiscount(orderPriceDto);
+        outputView.printEventBadge(promotionDto);
     }
 }
--- src/main/java/christmas/message/BenefitMessage.java
+++ src/main/java/christmas/message/BenefitMessage.java
@@ -1,11 +1,11 @@
 package christmas.message;
 
 public enum BenefitMessage {
-    CHRISTMAS_D_DAY_DISCOUNT_NAME("크리스마스 디데이 할인: %d원"),
-    WEEKDAY_DISCOUNT_NAME("평일 할인: %d원"),
-    WEEKEND_DISCOUNT_NAME("주말 할인: %d원"),
-    SPECIAL_DISCOUNT_NAME("특별 할인: %d원"),
-    GIVEAWAY_EVENT_NAME("증정 이벤트: %d원");
+    CHRISTMAS_D_DAY_DISCOUNT_MESSAGE("크리스마스 디데이 할인: %s원"),
+    WEEKDAY_DISCOUNT_MESSAGE("평일 할인: %s원"),
+    WEEKEND_DISCOUNT_MESSAGE("주말 할인: %s원"),
+    SPECIAL_DISCOUNT_MESSAGE("특별 할인: %s원"),
+    GIVEAWAY_EVENT_MESSAGE("증정 이벤트: %s원");
 
     private final String message;
     BenefitMessage(String value) {
