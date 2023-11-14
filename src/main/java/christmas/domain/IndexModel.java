d27d1b5c88cdc56c87c190100475fdaac5a393ce
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-14 22:30:34 +0900

    feat:직렬화를 위한 IndexModel 추가

---
+++ src/main/java/christmas/domain/IndexModel.java
@@ -0,0 +1,13 @@
+package christmas.domain;
+
+public abstract class IndexModel {
+    protected Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
